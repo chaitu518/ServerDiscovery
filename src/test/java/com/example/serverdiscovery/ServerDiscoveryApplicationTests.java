49de33b56434b9f346fa682c3187a061b91f3913
saichaitanya518 <saichaitanya518@gmail.com>
2024-09-17 23:00:42 +0530

    second commit : )

--- src/test/java/com/example/serverdiscovery/ServerDiscoveryApplicationTests.java
+++ src/test/java/com/example/serverdiscovery/ServerDiscoveryApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ServerDiscoveryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
